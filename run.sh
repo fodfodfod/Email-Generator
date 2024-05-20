b82cb480bc645360a447bb19d3a88147eb3a6505
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2024-05-20 09:22:42 -0400

    fix me being dum

--- run.sh
+++ run.sh
@@ -2,16 +2,18 @@
 #!/bin/bash
 
 # Check if python3 is installed
+cd $(dirname "$0")
 if command -v python3 &> /dev/null
 then
     python3 txt-to-json.py
     python3 json-to-email.py
 elif command -v python &> /dev/null
 then
-    python3 txt-to-json.py
-    python3 json-to-email.py
+    python txt-to-json.py
+    python json-to-email.py
 else
     echo "Error: Python is not installed."
     exit 1
 fi
 
+sleep 10000
