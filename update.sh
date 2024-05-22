147501784265bc4f0ee10febefef94d12ac6b80e
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2024-05-22 09:50:33 -0400

    redesign updater

--- update.sh
+++ update.sh
@@ -5,8 +5,21 @@ cd $(dirname "$0")
 REPO="fodfodfod/Email-Generator"
 BRANCH="main"
 
+# Define the GitHub URL for the file
+URL="https://raw.githubusercontent.com/$REPO/$BRANCH/update.sh"
+
+# Download and replace the file
+curl -s -o "update.sh" "$URL" && echo "Replaced updater"
+
+
+# Define the GitHub URL for the file
+URL="https://raw.githubusercontent.com/$REPO/$BRANCH/files.txt"
+
+# Download and replace the file
+curl -s -o "files.txt" "$URL" && echo "Replaced files list"
+
 # Find all Python scripts in the current directory
-find . -name "*.py" | while read -r file; do
+cat files.txt | while read -r file; do
   # Extract the relative path
   REL_PATH=${file#./}
 
@@ -17,8 +30,3 @@ find . -name "*.py" | while read -r file; do
   curl -s -o "$file" "$URL" && echo "Replaced $file"
 done
 
-# Define the GitHub URL for the file
-URL="https://raw.githubusercontent.com/$REPO/$BRANCH/update.sh"
-
-# Download and replace the file
-curl -s -o "update.sh" "$URL" && echo "Replaced updater"
